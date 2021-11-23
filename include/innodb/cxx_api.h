62f1cf0a1e249fbd867fbdc2f09816aa8cef4acd
xiaoma20082008 <chunxiao_ma@outlook.com>
2021-11-23 19:34:02 +0800

    <feat>: copy innodb from mysql-server

---
+++ include/innodb/cxx_api.h
@@ -0,0 +1,8 @@
+//
+// Created by chunxiao ma on 2021/11/23.
+//
+
+#ifndef INNODB_CXX_API_H
+#define INNODB_CXX_API_H
+
+#endif // INNODB_CXX_API_H
---
+++ src/main/main.cc
@@ -0,0 +1,5 @@
+//
+// Created by chunxiao ma on 2021/11/10.
+//
+
+int main(int argc, char **argv) { return 0; }
